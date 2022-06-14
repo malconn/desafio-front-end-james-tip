8aaf94c9a090290309218dc8f72b679398fdda77
malconn <malconaugusto@outlook.com>
2022-06-14 00:04:41 -0300

    feat: initial commit

---
+++ cadastro-produtos/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './modules/Home'
+import './index.css'
+import Cadastro from './modules/Cadastro';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+  
+)
---
+++ cadastro-produtos/src/modules/Cadastro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../utils/Navbar'
+const Cadastro = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Cadastro</h1>
+    </div>
+    
+  )
+}
+
+export default Cadastro
